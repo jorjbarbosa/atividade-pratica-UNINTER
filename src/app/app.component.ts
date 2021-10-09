86b86170ab9f2563391fa3e1c1cdd09f4a2b463a
Jorge Barbosa <jorgebarbosa_@outlook.com>
2021-10-08 22:05:22 -0400

    alterando local bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// importa o arquivo com os dados
+import { data } from './arquivo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'atividade-pratica';
+  // permite que os dados sejam acessados pelo template
+  alunos: any[] = data;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,33 @@
+// Define um array de Alunos para ser mostrado na tabela
+export const data: Array<any> = [
+  {
+    nome: 'Jorge Luis Abadias Barbosa',
+    ru: '3539708',
+    curso: 'Análise e desenvolvimento de sistemas',
+    dt_aniversario: '04/08/1996'
+  },
+  {
+    nome: 'Maria Cristina Santos',
+    ru: '8529145',
+    curso: 'Arquitetura',
+    dt_aniversario: '13/02/2000'
+  },
+  {
+    nome: 'Gabriely Souza',
+    ru: '4532982',
+    curso: 'Design de interiores',
+    dt_aniversario: '01/12/1998'
+  },
+  {
+    nome: 'Carlos Alberto da Silva',
+    ru: '5245521',
+    curso: 'Sistemas de informação',
+    dt_aniversario: '20/05/1995'
+  },
+  {
+    nome: 'John Doe',
+    ru: '1234421',
+    curso: 'Engenharia de Software',
+    dt_aniversario: '09/03/1994'
+  }
+];
